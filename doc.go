c33461e6645cfb722494de12b24f24eb164ab240
Mat Ryer <mat@stretchr.com>
2013-09-11 16:54:14 -0600

    added more docs

--- doc.go
+++ doc.go
@@ -2,8 +2,28 @@
 //
 // Reading data
 //
-//     obj, err := objx.FromJSON(`{"name":"Tyler","age":29,"tags":["musician","coder","singer"]}`)
-//     name := obj.Get("name").Str()
-//     age := obj.Get("age").Int()
-//     secondTag := obj.Get("tags[2]").Str()
+// Objx follows a fluent design allowing you to quickly and intuitively access
+// data from within otherwise difficult or deep nested places.
+//
+//     // parse some JSON
+//     data := objx.MustFromJSON(`{
+//       "name": "Tyler",
+//       "age": 29,
+//       "tags": ["musician","coder","singer"],
+//       "deep": {
+//	       "nested": {
+//	         item: "easy"
+//         }
+//       }
+//     }`)
+//
+//     // get some fields
+//     name := data.Get("name").Str()
+//     age := data.Get("age").Int()
+//
+//     // access an array element
+//     secondTag := data.Get("tags[2]").Str()
+//
+//     // get at some deep data using dot notation
+//     deepItem := data.Get("deep.nested.item").Str()
 package objx
