56bcdd35419e726ab2b809c5bdbe4f5987a33172
Mat Ryer <mat@stretchr.com>
2013-09-11 11:49:39 -0600

    added slice types and musts

--- getters.go
+++ getters.go
@@ -358,3 +358,307 @@ func (o *O) Complex128(optionalDefault ...complex128) complex128 {
 func (o *O) MustComplex128() complex128 {
 	return o.obj.(complex128)
 }
+
+// BoolSlice gets the value as a []bool, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) BoolSlice(optionalDefault ...[]bool) []bool {
+	if s, ok := o.obj.([]bool); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return nil
+}
+
+// BoolSlice gets the value as a []bool.
+//
+// Panics if the object is not a []bool.
+func (o *O) MustBoolSlice() []bool {
+	return o.obj.([]bool)
+}
+
+// IntSlice gets the value as a []int, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) IntSlice(optionalDefault ...[]int) []int {
+	if s, ok := o.obj.([]int); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return nil
+}
+
+// IntSlice gets the value as a []int.
+//
+// Panics if the object is not a []int.
+func (o *O) MustIntSlice() []int {
+	return o.obj.([]int)
+}
+
+// Int8Slice gets the value as a []int8, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Int8Slice(optionalDefault ...[]int8) []int8 {
+	if s, ok := o.obj.([]int8); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return nil
+}
+
+// Int8Slice gets the value as a []int8.
+//
+// Panics if the object is not a []int8.
+func (o *O) MustInt8Slice() []int8 {
+	return o.obj.([]int8)
+}
+
+// Int16Slice gets the value as a []int16, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Int16Slice(optionalDefault ...[]int16) []int16 {
+	if s, ok := o.obj.([]int16); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return nil
+}
+
+// Int16Slice gets the value as a []int16.
+//
+// Panics if the object is not a []int16.
+func (o *O) MustInt16Slice() []int16 {
+	return o.obj.([]int16)
+}
+
+// Int32Slice gets the value as a []int32, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Int32Slice(optionalDefault ...[]int32) []int32 {
+	if s, ok := o.obj.([]int32); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return nil
+}
+
+// Int32Slice gets the value as a []int32.
+//
+// Panics if the object is not a []int32.
+func (o *O) MustInt32Slice() []int32 {
+	return o.obj.([]int32)
+}
+
+// Int64Slice gets the value as a []int64, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Int64Slice(optionalDefault ...[]int64) []int64 {
+	if s, ok := o.obj.([]int64); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return nil
+}
+
+// Int64Slice gets the value as a []int64.
+//
+// Panics if the object is not a []int64.
+func (o *O) MustInt64Slice() []int64 {
+	return o.obj.([]int64)
+}
+
+// UintSlice gets the value as a []uint, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) UintSlice(optionalDefault ...[]uint) []uint {
+	if s, ok := o.obj.([]uint); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return nil
+}
+
+// UintSlice gets the value as a []uint.
+//
+// Panics if the object is not a []uint.
+func (o *O) MustUintSlice() []uint {
+	return o.obj.([]uint)
+}
+
+// Uint8Slice gets the value as a []uint8, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Uint8Slice(optionalDefault ...[]uint8) []uint8 {
+	if s, ok := o.obj.([]uint8); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return nil
+}
+
+// Uint8Slice gets the value as a []uint8.
+//
+// Panics if the object is not a []uint8.
+func (o *O) MustUint8Slice() []uint8 {
+	return o.obj.([]uint8)
+}
+
+// Uint16Slice gets the value as a []uint16, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Uint16Slice(optionalDefault ...[]uint16) []uint16 {
+	if s, ok := o.obj.([]uint16); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return nil
+}
+
+// Uint16Slice gets the value as a []uint16.
+//
+// Panics if the object is not a []uint16.
+func (o *O) MustUint16Slice() []uint16 {
+	return o.obj.([]uint16)
+}
+
+// Uint32Slice gets the value as a []uint32, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Uint32Slice(optionalDefault ...[]uint32) []uint32 {
+	if s, ok := o.obj.([]uint32); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return nil
+}
+
+// Uint32Slice gets the value as a []uint32.
+//
+// Panics if the object is not a []uint32.
+func (o *O) MustUint32Slice() []uint32 {
+	return o.obj.([]uint32)
+}
+
+// Uint64Slice gets the value as a []uint64, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Uint64Slice(optionalDefault ...[]uint64) []uint64 {
+	if s, ok := o.obj.([]uint64); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return nil
+}
+
+// Uint64Slice gets the value as a []uint64.
+//
+// Panics if the object is not a []uint64.
+func (o *O) MustUint64Slice() []uint64 {
+	return o.obj.([]uint64)
+}
+
+// UintptrSlice gets the value as a []uintptr, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) UintptrSlice(optionalDefault ...[]uintptr) []uintptr {
+	if s, ok := o.obj.([]uintptr); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return nil
+}
+
+// UintptrSlice gets the value as a []uintptr.
+//
+// Panics if the object is not a []uintptr.
+func (o *O) MustUintptrSlice() []uintptr {
+	return o.obj.([]uintptr)
+}
+
+// Float32Slice gets the value as a []float32, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Float32Slice(optionalDefault ...[]float32) []float32 {
+	if s, ok := o.obj.([]float32); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return nil
+}
+
+// Float32Slice gets the value as a []float32.
+//
+// Panics if the object is not a []float32.
+func (o *O) MustFloat32Slice() []float32 {
+	return o.obj.([]float32)
+}
+
+// Float64Slice gets the value as a []float64, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Float64Slice(optionalDefault ...[]float64) []float64 {
+	if s, ok := o.obj.([]float64); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return nil
+}
+
+// Float64Slice gets the value as a []float64.
+//
+// Panics if the object is not a []float64.
+func (o *O) MustFloat64Slice() []float64 {
+	return o.obj.([]float64)
+}
+
+// Complex64Slice gets the value as a []complex64, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Complex64Slice(optionalDefault ...[]complex64) []complex64 {
+	if s, ok := o.obj.([]complex64); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return nil
+}
+
+// Complex64Slice gets the value as a []complex64.
+//
+// Panics if the object is not a []complex64.
+func (o *O) MustComplex64Slice() []complex64 {
+	return o.obj.([]complex64)
+}
+
+// Complex128Slice gets the value as a []complex128, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Complex128Slice(optionalDefault ...[]complex128) []complex128 {
+	if s, ok := o.obj.([]complex128); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return nil
+}
+
+// Complex128Slice gets the value as a []complex128.
+//
+// Panics if the object is not a []complex128.
+func (o *O) MustComplex128Slice() []complex128 {
+	return o.obj.([]complex128)
+}
--- getters_test.go
+++ getters_test.go
@@ -227,3 +227,196 @@ func TestComplex128(t *testing.T) {
 	})
 
 }
+
+func TestBoolSlice(t *testing.T) {
+
+	m := map[string]interface{}{"value": []bool{bool(true)}, "nothing": nil}
+	assert.Equal(t, true, New(m).Get("value").BoolSlice()[0])
+	assert.Equal(t, []bool(nil), New(m).Get("nothing").BoolSlice())
+	assert.Equal(t, true, New(m).Get("nothing").BoolSlice([]bool{true})[0])
+
+	assert.Panics(t, func() {
+		New(m).Get("nothing").MustBool()
+	})
+
+}
+func TestIntSlice(t *testing.T) {
+
+	m := map[string]interface{}{"value": []int{int(1)}, "nothing": nil}
+	assert.Equal(t, int(1), New(m).Get("value").IntSlice()[0])
+	assert.Equal(t, []int(nil), New(m).Get("nothing").IntSlice())
+	assert.Equal(t, int(123), New(m).Get("nothing").IntSlice([]int{123})[0])
+
+	assert.Panics(t, func() {
+		New(m).Get("nothing").MustInt()
+	})
+
+}
+func TestInt8Slice(t *testing.T) {
+
+	m := map[string]interface{}{"value": []int8{int8(1)}, "nothing": nil}
+	assert.Equal(t, int8(1), New(m).Get("value").Int8Slice()[0])
+	assert.Equal(t, []int8(nil), New(m).Get("nothing").Int8Slice())
+	assert.Equal(t, int8(123), New(m).Get("nothing").Int8Slice([]int8{123})[0])
+
+	assert.Panics(t, func() {
+		New(m).Get("nothing").MustInt8()
+	})
+
+}
+func TestInt16Slice(t *testing.T) {
+
+	m := map[string]interface{}{"value": []int16{int16(1)}, "nothing": nil}
+	assert.Equal(t, int16(1), New(m).Get("value").Int16Slice()[0])
+	assert.Equal(t, []int16(nil), New(m).Get("nothing").Int16Slice())
+	assert.Equal(t, int16(123), New(m).Get("nothing").Int16Slice([]int16{123})[0])
+
+	assert.Panics(t, func() {
+		New(m).Get("nothing").MustInt16()
+	})
+
+}
+func TestInt32Slice(t *testing.T) {
+
+	m := map[string]interface{}{"value": []int32{int32(1)}, "nothing": nil}
+	assert.Equal(t, int32(1), New(m).Get("value").Int32Slice()[0])
+	assert.Equal(t, []int32(nil), New(m).Get("nothing").Int32Slice())
+	assert.Equal(t, int32(123), New(m).Get("nothing").Int32Slice([]int32{123})[0])
+
+	assert.Panics(t, func() {
+		New(m).Get("nothing").MustInt32()
+	})
+
+}
+func TestInt64Slice(t *testing.T) {
+
+	m := map[string]interface{}{"value": []int64{int64(1)}, "nothing": nil}
+	assert.Equal(t, int64(1), New(m).Get("value").Int64Slice()[0])
+	assert.Equal(t, []int64(nil), New(m).Get("nothing").Int64Slice())
+	assert.Equal(t, int64(123), New(m).Get("nothing").Int64Slice([]int64{123})[0])
+
+	assert.Panics(t, func() {
+		New(m).Get("nothing").MustInt64()
+	})
+
+}
+func TestUintSlice(t *testing.T) {
+
+	m := map[string]interface{}{"value": []uint{uint(1)}, "nothing": nil}
+	assert.Equal(t, uint(1), New(m).Get("value").UintSlice()[0])
+	assert.Equal(t, []uint(nil), New(m).Get("nothing").UintSlice())
+	assert.Equal(t, uint(123), New(m).Get("nothing").UintSlice([]uint{123})[0])
+
+	assert.Panics(t, func() {
+		New(m).Get("nothing").MustUint()
+	})
+
+}
+func TestUint8Slice(t *testing.T) {
+
+	m := map[string]interface{}{"value": []uint8{uint8(1)}, "nothing": nil}
+	assert.Equal(t, uint8(1), New(m).Get("value").Uint8Slice()[0])
+	assert.Equal(t, []uint8(nil), New(m).Get("nothing").Uint8Slice())
+	assert.Equal(t, uint8(123), New(m).Get("nothing").Uint8Slice([]uint8{123})[0])
+
+	assert.Panics(t, func() {
+		New(m).Get("nothing").MustUint8()
+	})
+
+}
+func TestUint16Slice(t *testing.T) {
+
+	m := map[string]interface{}{"value": []uint16{uint16(1)}, "nothing": nil}
+	assert.Equal(t, uint16(1), New(m).Get("value").Uint16Slice()[0])
+	assert.Equal(t, []uint16(nil), New(m).Get("nothing").Uint16Slice())
+	assert.Equal(t, uint16(123), New(m).Get("nothing").Uint16Slice([]uint16{123})[0])
+
+	assert.Panics(t, func() {
+		New(m).Get("nothing").MustUint16()
+	})
+
+}
+func TestUint32Slice(t *testing.T) {
+
+	m := map[string]interface{}{"value": []uint32{uint32(1)}, "nothing": nil}
+	assert.Equal(t, uint32(1), New(m).Get("value").Uint32Slice()[0])
+	assert.Equal(t, []uint32(nil), New(m).Get("nothing").Uint32Slice())
+	assert.Equal(t, uint32(123), New(m).Get("nothing").Uint32Slice([]uint32{123})[0])
+
+	assert.Panics(t, func() {
+		New(m).Get("nothing").MustUint32()
+	})
+
+}
+func TestUint64Slice(t *testing.T) {
+
+	m := map[string]interface{}{"value": []uint64{uint64(1)}, "nothing": nil}
+	assert.Equal(t, uint64(1), New(m).Get("value").Uint64Slice()[0])
+	assert.Equal(t, []uint64(nil), New(m).Get("nothing").Uint64Slice())
+	assert.Equal(t, uint64(123), New(m).Get("nothing").Uint64Slice([]uint64{123})[0])
+
+	assert.Panics(t, func() {
+		New(m).Get("nothing").MustUint64()
+	})
+
+}
+func TestUintptrSlice(t *testing.T) {
+
+	m := map[string]interface{}{"value": []uintptr{uintptr(1)}, "nothing": nil}
+	assert.Equal(t, uintptr(1), New(m).Get("value").UintptrSlice()[0])
+	assert.Equal(t, []uintptr(nil), New(m).Get("nothing").UintptrSlice())
+	assert.Equal(t, uintptr(123), New(m).Get("nothing").UintptrSlice([]uintptr{123})[0])
+
+	assert.Panics(t, func() {
+		New(m).Get("nothing").MustUintptr()
+	})
+
+}
+func TestFloat32Slice(t *testing.T) {
+
+	m := map[string]interface{}{"value": []float32{float32(1)}, "nothing": nil}
+	assert.Equal(t, float32(1), New(m).Get("value").Float32Slice()[0])
+	assert.Equal(t, []float32(nil), New(m).Get("nothing").Float32Slice())
+	assert.Equal(t, float32(123), New(m).Get("nothing").Float32Slice([]float32{123})[0])
+
+	assert.Panics(t, func() {
+		New(m).Get("nothing").MustFloat32()
+	})
+
+}
+func TestFloat64Slice(t *testing.T) {
+
+	m := map[string]interface{}{"value": []float64{float64(1)}, "nothing": nil}
+	assert.Equal(t, float64(1), New(m).Get("value").Float64Slice()[0])
+	assert.Equal(t, []float64(nil), New(m).Get("nothing").Float64Slice())
+	assert.Equal(t, float64(123), New(m).Get("nothing").Float64Slice([]float64{123})[0])
+
+	assert.Panics(t, func() {
+		New(m).Get("nothing").MustFloat64()
+	})
+
+}
+func TestComplex64Slice(t *testing.T) {
+
+	m := map[string]interface{}{"value": []complex64{complex64(1)}, "nothing": nil}
+	assert.Equal(t, complex64(1), New(m).Get("value").Complex64Slice()[0])
+	assert.Equal(t, []complex64(nil), New(m).Get("nothing").Complex64Slice())
+	assert.Equal(t, complex64(123), New(m).Get("nothing").Complex64Slice([]complex64{123})[0])
+
+	assert.Panics(t, func() {
+		New(m).Get("nothing").MustComplex64()
+	})
+
+}
+func TestComplex128Slice(t *testing.T) {
+
+	m := map[string]interface{}{"value": []complex128{complex128(1)}, "nothing": nil}
+	assert.Equal(t, complex128(1), New(m).Get("value").Complex128Slice()[0])
+	assert.Equal(t, []complex128(nil), New(m).Get("nothing").Complex128Slice())
+	assert.Equal(t, complex128(123), New(m).Get("nothing").Complex128Slice([]complex128{123})[0])
+
+	assert.Panics(t, func() {
+		New(m).Get("nothing").MustComplex128()
+	})
+
+}
