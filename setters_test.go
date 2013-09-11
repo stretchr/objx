75ee0a1eee80fe53d34ba74f5ff85857ab0f191b
Mat Ryer <mat@stretchr.com>
2013-09-11 10:59:18 -0600

    added test for setting nil

--- setters_test.go
+++ setters_test.go
@@ -23,6 +23,15 @@ func TestSetInDeepMap(t *testing.T) {
 
 }
 
+func TestSetNilInDeepMap(t *testing.T) {
+
+	m := map[string]interface{}{"one": map[string]interface{}{"two": map[string]interface{}{"three": 3}}}
+	o := New(m)
+	assert.Equal(t, o, o.Set("one.two.three", nil), "Set should chain")
+	assert.Equal(t, nil, m["one"].(map[string]interface{})["two"].(map[string]interface{})["three"])
+
+}
+
 func TestSetInArray(t *testing.T) {
 
 	a := []string{"one", "two", "three"}
