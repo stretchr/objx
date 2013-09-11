c46f81aca54f59be13fec4d21f57dc851ae6b303
Mat Ryer <mat@stretchr.com>
2013-09-11 11:34:04 -0600

    added type specific getters (along with Must counterparts)

--- getters.go
+++ getters.go
@@ -36,7 +36,325 @@ func (o *O) String() string {
 
 }
 
-// Str gets the string contained.
-func (o *O) Str() string {
+// Str gets the value as a string, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Str(optionalDefault ...string) string {
+	if s, ok := o.obj.(string); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return ""
+}
+
+// Str gets the value as a string.
+//
+// Panics if the object is not a string.
+func (o *O) MustStr() string {
 	return o.obj.(string)
+}
+
+// Bool gets the value as a bool, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Bool(optionalDefault ...bool) bool {
+	if s, ok := o.obj.(bool); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return false
+}
+
+// Bool gets the value as a bool.
+//
+// Panics if the object is not a bool.
+func (o *O) MustBool() bool {
+	return o.obj.(bool)
+}
+
+// Int gets the value as a int, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Int(optionalDefault ...int) int {
+	if s, ok := o.obj.(int); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return 0
+}
+
+// Int gets the value as a int.
+//
+// Panics if the object is not a int.
+func (o *O) MustInt() int {
+	return o.obj.(int)
+}
+
+// Int8 gets the value as a int8, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Int8(optionalDefault ...int8) int8 {
+	if s, ok := o.obj.(int8); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return 0
+}
+
+// Int8 gets the value as a int8.
+//
+// Panics if the object is not a int8.
+func (o *O) MustInt8() int8 {
+	return o.obj.(int8)
+}
+
+// Int16 gets the value as a int16, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Int16(optionalDefault ...int16) int16 {
+	if s, ok := o.obj.(int16); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return 0
+}
+
+// Int16 gets the value as a int16.
+//
+// Panics if the object is not a int16.
+func (o *O) MustInt16() int16 {
+	return o.obj.(int16)
+}
+
+// Int32 gets the value as a int32, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Int32(optionalDefault ...int32) int32 {
+	if s, ok := o.obj.(int32); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return 0
+}
+
+// Int32 gets the value as a int32.
+//
+// Panics if the object is not a int32.
+func (o *O) MustInt32() int32 {
+	return o.obj.(int32)
+}
+
+// Int64 gets the value as a int64, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Int64(optionalDefault ...int64) int64 {
+	if s, ok := o.obj.(int64); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return 0
+}
+
+// Int64 gets the value as a int64.
+//
+// Panics if the object is not a int64.
+func (o *O) MustInt64() int64 {
+	return o.obj.(int64)
+}
+
+// Uint gets the value as a uint, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Uint(optionalDefault ...uint) uint {
+	if s, ok := o.obj.(uint); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return 0
+}
+
+// Uint gets the value as a uint.
+//
+// Panics if the object is not a uint.
+func (o *O) MustUint() uint {
+	return o.obj.(uint)
+}
+
+// Uint8 gets the value as a uint8, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Uint8(optionalDefault ...uint8) uint8 {
+	if s, ok := o.obj.(uint8); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return 0
+}
+
+// Uint8 gets the value as a uint8.
+//
+// Panics if the object is not a uint8.
+func (o *O) MustUint8() uint8 {
+	return o.obj.(uint8)
+}
+
+// Uint16 gets the value as a uint16, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Uint16(optionalDefault ...uint16) uint16 {
+	if s, ok := o.obj.(uint16); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return 0
+}
+
+// Uint16 gets the value as a uint16.
+//
+// Panics if the object is not a uint16.
+func (o *O) MustUint16() uint16 {
+	return o.obj.(uint16)
+}
+
+// Uint32 gets the value as a uint32, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Uint32(optionalDefault ...uint32) uint32 {
+	if s, ok := o.obj.(uint32); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return 0
+}
+
+// Uint32 gets the value as a uint32.
+//
+// Panics if the object is not a uint32.
+func (o *O) MustUint32() uint32 {
+	return o.obj.(uint32)
+}
+
+// Uint64 gets the value as a uint64, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Uint64(optionalDefault ...uint64) uint64 {
+	if s, ok := o.obj.(uint64); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return 0
+}
+
+// Uint64 gets the value as a uint64.
+//
+// Panics if the object is not a uint64.
+func (o *O) MustUint64() uint64 {
+	return o.obj.(uint64)
+}
+
+// Uintptr gets the value as a uintptr, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Uintptr(optionalDefault ...uintptr) uintptr {
+	if s, ok := o.obj.(uintptr); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return 0
+}
+
+// Uintptr gets the value as a uintptr.
+//
+// Panics if the object is not a uintptr.
+func (o *O) MustUintptr() uintptr {
+	return o.obj.(uintptr)
+}
+
+// Float32 gets the value as a float32, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Float32(optionalDefault ...float32) float32 {
+	if s, ok := o.obj.(float32); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return 0
+}
+
+// Float32 gets the value as a float32.
+//
+// Panics if the object is not a float32.
+func (o *O) MustFloat32() float32 {
+	return o.obj.(float32)
+}
+
+// Float64 gets the value as a float64, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Float64(optionalDefault ...float64) float64 {
+	if s, ok := o.obj.(float64); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return 0
+}
+
+// Float64 gets the value as a float64.
+//
+// Panics if the object is not a float64.
+func (o *O) MustFloat64() float64 {
+	return o.obj.(float64)
+}
+
+// Complex64 gets the value as a complex64, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Complex64(optionalDefault ...complex64) complex64 {
+	if s, ok := o.obj.(complex64); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return 0
+}
+
+// Complex64 gets the value as a complex64.
+//
+// Panics if the object is not a complex64.
+func (o *O) MustComplex64() complex64 {
+	return o.obj.(complex64)
+}
+
+// Complex128 gets the value as a complex128, returns the optinoalDefault
+// value or a system default object if the value is the wrong type.
+func (o *O) Complex128(optionalDefault ...complex128) complex128 {
+	if s, ok := o.obj.(complex128); ok {
+		return s
+	}
+	if len(optionalDefault) == 1 {
+		return optionalDefault[0]
+	}
+	return 0
+}
+
+// Complex128 gets the value as a complex128.
+//
+// Panics if the object is not a complex128.
+func (o *O) MustComplex128() complex128 {
+	return o.obj.(complex128)
 }
--- getters_test.go
+++ getters_test.go
@@ -26,10 +26,204 @@ func TestStr(t *testing.T) {
 
 	m := map[string]interface{}{"name": "Mat", "age": 29}
 	assert.Equal(t, "Mat", New(m).Get("name").Str())
-	assert.Equal(t, "29", New(m).Get("age").Str())
+	assert.Equal(t, "", New(m).Get("age").Str())
+	assert.Equal(t, "def", New(m).Get("age").Str("def"))
 
 	assert.Panics(t, func() {
 		New(m).Get("age").MustStr()
+	})
+
+}
+
+func TestBool(t *testing.T) {
+
+	m := map[string]interface{}{"value": bool(true), "nothing": nil}
+	assert.Equal(t, bool(true), New(m).Get("value").Bool())
+	assert.Equal(t, bool(false), New(m).Get("nothing").Bool(false))
+	assert.Equal(t, bool(true), New(m).Get("nothing").Bool(true))
+
+	assert.Panics(t, func() {
+		New(m).Get("age").MustBool()
+	})
+
+}
+func TestInt(t *testing.T) {
+
+	m := map[string]interface{}{"value": int(1), "nothing": nil}
+	assert.Equal(t, int(1), New(m).Get("value").Int())
+	assert.Equal(t, int(0), New(m).Get("nothing").Int())
+	assert.Equal(t, int(123), New(m).Get("nothing").Int(123))
+
+	assert.Panics(t, func() {
+		New(m).Get("age").MustInt()
+	})
+
+}
+func TestInt8(t *testing.T) {
+
+	m := map[string]interface{}{"value": int8(1), "nothing": nil}
+	assert.Equal(t, int8(1), New(m).Get("value").Int8())
+	assert.Equal(t, int8(0), New(m).Get("nothing").Int8())
+	assert.Equal(t, int8(123), New(m).Get("nothing").Int8(123))
+
+	assert.Panics(t, func() {
+		New(m).Get("age").MustInt8()
+	})
+
+}
+func TestInt16(t *testing.T) {
+
+	m := map[string]interface{}{"value": int16(1), "nothing": nil}
+	assert.Equal(t, int16(1), New(m).Get("value").Int16())
+	assert.Equal(t, int16(0), New(m).Get("nothing").Int16())
+	assert.Equal(t, int16(123), New(m).Get("nothing").Int16(123))
+
+	assert.Panics(t, func() {
+		New(m).Get("age").MustInt16()
+	})
+
+}
+func TestInt32(t *testing.T) {
+
+	m := map[string]interface{}{"value": int32(1), "nothing": nil}
+	assert.Equal(t, int32(1), New(m).Get("value").Int32())
+	assert.Equal(t, int32(0), New(m).Get("nothing").Int32())
+	assert.Equal(t, int32(123), New(m).Get("nothing").Int32(123))
+
+	assert.Panics(t, func() {
+		New(m).Get("age").MustInt32()
+	})
+
+}
+func TestInt64(t *testing.T) {
+
+	m := map[string]interface{}{"value": int64(1), "nothing": nil}
+	assert.Equal(t, int64(1), New(m).Get("value").Int64())
+	assert.Equal(t, int64(0), New(m).Get("nothing").Int64())
+	assert.Equal(t, int64(123), New(m).Get("nothing").Int64(123))
+
+	assert.Panics(t, func() {
+		New(m).Get("age").MustInt64()
+	})
+
+}
+func TestUint(t *testing.T) {
+
+	m := map[string]interface{}{"value": uint(1), "nothing": nil}
+	assert.Equal(t, uint(1), New(m).Get("value").Uint())
+	assert.Equal(t, uint(0), New(m).Get("nothing").Uint())
+	assert.Equal(t, uint(123), New(m).Get("nothing").Uint(123))
+
+	assert.Panics(t, func() {
+		New(m).Get("age").MustUint()
+	})
+
+}
+func TestUint8(t *testing.T) {
+
+	m := map[string]interface{}{"value": uint8(1), "nothing": nil}
+	assert.Equal(t, uint8(1), New(m).Get("value").Uint8())
+	assert.Equal(t, uint8(0), New(m).Get("nothing").Uint8())
+	assert.Equal(t, uint8(123), New(m).Get("nothing").Uint8(123))
+
+	assert.Panics(t, func() {
+		New(m).Get("age").MustUint8()
+	})
+
+}
+func TestUint16(t *testing.T) {
+
+	m := map[string]interface{}{"value": uint16(1), "nothing": nil}
+	assert.Equal(t, uint16(1), New(m).Get("value").Uint16())
+	assert.Equal(t, uint16(0), New(m).Get("nothing").Uint16())
+	assert.Equal(t, uint16(123), New(m).Get("nothing").Uint16(123))
+
+	assert.Panics(t, func() {
+		New(m).Get("age").MustUint16()
+	})
+
+}
+func TestUint32(t *testing.T) {
+
+	m := map[string]interface{}{"value": uint32(1), "nothing": nil}
+	assert.Equal(t, uint32(1), New(m).Get("value").Uint32())
+	assert.Equal(t, uint32(0), New(m).Get("nothing").Uint32())
+	assert.Equal(t, uint32(123), New(m).Get("nothing").Uint32(123))
+
+	assert.Panics(t, func() {
+		New(m).Get("age").MustUint32()
+	})
+
+}
+func TestUint64(t *testing.T) {
+
+	m := map[string]interface{}{"value": uint64(1), "nothing": nil}
+	assert.Equal(t, uint64(1), New(m).Get("value").Uint64())
+	assert.Equal(t, uint64(0), New(m).Get("nothing").Uint64())
+	assert.Equal(t, uint64(123), New(m).Get("nothing").Uint64(123))
+
+	assert.Panics(t, func() {
+		New(m).Get("age").MustUint64()
+	})
+
+}
+func TestUintptr(t *testing.T) {
+
+	m := map[string]interface{}{"value": uintptr(1), "nothing": nil}
+	assert.Equal(t, uintptr(1), New(m).Get("value").Uintptr())
+	assert.Equal(t, uintptr(0), New(m).Get("nothing").Uintptr())
+	assert.Equal(t, uintptr(123), New(m).Get("nothing").Uintptr(123))
+
+	assert.Panics(t, func() {
+		New(m).Get("age").MustUintptr()
+	})
+
+}
+func TestFloat32(t *testing.T) {
+
+	m := map[string]interface{}{"value": float32(1), "nothing": nil}
+	assert.Equal(t, float32(1), New(m).Get("value").Float32())
+	assert.Equal(t, float32(0), New(m).Get("nothing").Float32())
+	assert.Equal(t, float32(123), New(m).Get("nothing").Float32(123))
+
+	assert.Panics(t, func() {
+		New(m).Get("age").MustFloat32()
+	})
+
+}
+func TestFloat64(t *testing.T) {
+
+	m := map[string]interface{}{"value": float64(1), "nothing": nil}
+	assert.Equal(t, float64(1), New(m).Get("value").Float64())
+	assert.Equal(t, float64(0), New(m).Get("nothing").Float64())
+	assert.Equal(t, float64(123), New(m).Get("nothing").Float64(123))
+
+	assert.Panics(t, func() {
+		New(m).Get("age").MustFloat64()
+	})
+
+}
+func TestComplex64(t *testing.T) {
+
+	m := map[string]interface{}{"value": complex64(1), "nothing": nil}
+	assert.Equal(t, complex64(1), New(m).Get("value").Complex64())
+	assert.Equal(t, complex64(0), New(m).Get("nothing").Complex64())
+	assert.Equal(t, complex64(123), New(m).Get("nothing").Complex64(123))
+
+	assert.Panics(t, func() {
+		New(m).Get("age").MustComplex64()
+	})
+
+}
+func TestComplex128(t *testing.T) {
+
+	m := map[string]interface{}{"value": complex128(1), "nothing": nil}
+	assert.Equal(t, complex128(1), New(m).Get("value").Complex128())
+	assert.Equal(t, complex128(0), New(m).Get("nothing").Complex128())
+	assert.Equal(t, complex128(123), New(m).Get("nothing").Complex128(123))
+
+	assert.Panics(t, func() {
+		New(m).Get("age").MustComplex128()
 	})
 
 }
