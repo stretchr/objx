3891a2932605a50de69ae62d59114478166f94af
Mat Ryer <mat@stretchr.com>
2013-09-11 16:47:20 -0600

    started enhancing docs

--- doc.go
+++ doc.go
@@ -1,2 +1,9 @@
 // Objx is a package for dealing with maps, slices, JSON and other data in Go.
+//
+// Reading data
+//
+//     obj, err := objx.FromJSON(`{"name":"Tyler","age":29,"tags":["musician","coder","singer"]}`)
+//     name := obj.Get("name").Str()
+//     age := obj.Get("age").Int()
+//     secondTag := obj.Get("tags[2]").Str()
 package objx
