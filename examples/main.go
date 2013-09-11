5881f65101ce9324cc81125b88432895172792ec
Mat Ryer <mat@stretchr.com>
2013-09-11 10:50:18 -0600

    added more example code

--- examples/main.go
+++ examples/main.go
@@ -16,6 +16,8 @@ func main() {
 	o := objx.MustJson(`{"name":"Mat"}`)
 
 	p("Obj: %s", o)
+	p(`  o.IsMap() = %v`, o.IsMap())
+	p(`  o.IsList() = %v`, o.IsList())
 	p(`  o.Get("name") = %s`, o.Get("name"))
 	p(`  o.Set("name", "Tyler")`)
 	o.Set("name", "Tyler")
@@ -23,6 +25,8 @@ func main() {
 	p(`  o.Set("age", 29)`)
 	o.Set("age", 29)
 	p("  Obj: %s", o)
+	p(`  o.Get("name").IsString() = %v`, o.Get("name").IsString())
+	p(`  o.Get("name").IsInt() = %v`, o.Get("name").IsInt())
 
 	fmt.Println()
 	fmt.Println()
