5d4502322145561cd3fe99a77d2eb46e15a40cf5
Mat Ryer <mat@stretchr.com>
2013-09-11 17:06:54 -0600

    added docs

--- doc.go
+++ doc.go
@@ -26,4 +26,18 @@
 //
 //     // get at some deep data using dot notation
 //     deepItem := data.Get("deep.nested.item").Str()
+//
+// Type strong
+//
+// Objx is type strong, meaning that it attempts to remove the need to
+// work so loosely with interface{}.  In that spirit, it providers
+// strongly typed accessors that make reading data easy.
+//
+// Example: Int(), Str(), Bool(), Float64(), UInt64() etc.
+//
+// If the data is missing or of the wrong type, the methods will
+// return a default value, or optionally, you can specify the default value.
+//
+// Objx also providers stricter Must methods for each supported type (like MustStr(),
+// MustBool(), MustInt(), MustFloat64() etc.)
 package objx
