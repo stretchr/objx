b5193c65f328f6c35baf148c18df479b446cc83d
Mat Ryer <mat@stretchr.com>
2013-09-11 10:08:35 -0600

    added test for setting inside array

--- setters_test.go
+++ setters_test.go
@@ -14,3 +14,16 @@ func TestSet(t *testing.T) {
 	assert.Equal(t, "Tyler", m["name"])
 
 }
+
+func TestSetInArray(t *testing.T) {
+
+	a := []string{"one", "two", "three"}
+	o := New(a)
+
+	o.Set(0, "1").Set(1, "2").Set(2, "3")
+
+	assert.Equal(t, "1", a[0])
+	assert.Equal(t, "2", a[1])
+	assert.Equal(t, "3", a[2])
+
+}
