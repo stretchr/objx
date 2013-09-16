70eb72eb1bf7fae3f23a6e0e8459361869a6da12
Tyler Bunnell <tylerbunnell@gmail.com>
2013-09-16 09:57:04 -0600

    Add test for Get
    The rest of the Get functionality is tested with the accessor tests.

--- accessors_test.go
+++ accessors_test.go
@@ -5,26 +5,26 @@ import (
 	"testing"
 )
 
-func TestAccessGetSingleField(t *testing.T) {
+func TestAccessorsAccessGetSingleField(t *testing.T) {
 
 	current := map[string]interface{}{"name": "Tyler"}
 	assert.Equal(t, "Tyler", access(current, "name", true))
 
 }
-func TestAccessGetDeep(t *testing.T) {
+func TestAccessorsAccessGetDeep(t *testing.T) {
 
 	current := map[string]interface{}{"name": map[string]interface{}{"first": "Tyler", "last": "Bunnell"}}
 	assert.Equal(t, "Tyler", access(current, "name.first", true))
 	assert.Equal(t, "Bunnell", access(current, "name.last", true))
 
 }
-func TestAccessGetDeepDeep(t *testing.T) {
+func TestAccessorsAccessGetDeepDeep(t *testing.T) {
 
 	current := map[string]interface{}{"one": map[string]interface{}{"two": map[string]interface{}{"three": map[string]interface{}{"four": 4}}}}
 	assert.Equal(t, 4, access(current, "one.two.three.four", true))
 
 }
-func TestAccessGetInsideArray(t *testing.T) {
+func TestAccessorsAccessGetInsideArray(t *testing.T) {
 
 	current := map[string]interface{}{"names": []interface{}{map[string]interface{}{"first": "Tyler", "last": "Bunnell"}, map[string]interface{}{"first": "Capitol", "last": "Bollocks"}}}
 	assert.Equal(t, "Tyler", access(current, "names[0].first", true))
@@ -39,7 +39,7 @@ func TestAccessGetInsideArray(t *testing.T) {
 
 }
 
-func TestAccessGetFromArrayWithInt(t *testing.T) {
+func TestAccessorsAccessGetFromArrayWithInt(t *testing.T) {
 
 	current := []interface{}{map[string]interface{}{"first": "Tyler", "last": "Bunnell"}, map[string]interface{}{"first": "Capitol", "last": "Bollocks"}}
 	one := access(current, 0, false)
@@ -49,5 +49,12 @@ func TestAccessGetFromArrayWithInt(t *testing.T) {
 	assert.Equal(t, "Tyler", one.(map[string]interface{})["first"])
 	assert.Equal(t, "Capitol", two.(map[string]interface{})["first"])
 	assert.Nil(t, three)
+
+}
+
+func TestAccessorsGet(t *testing.T) {
+
+	current := New(map[string]interface{}{"name": "Tyler"})
+	assert.Equal(t, "Tyler", current.Get("name").data)
 
 }
