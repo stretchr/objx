aea7ef1fcf3f43f3a3d1a05a3d6c8a304dd134ba
Ben Schumacher <hanzei@mailbox.org>
2018-01-25 10:09:11 +0100

    Consolidate files

--- map_test.go
+++ map_test.go
@@ -6,6 +6,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var TestMap = map[string]interface{}{
+	"name": "Tyler",
+	"address": map[string]interface{}{
+		"city":  "Salt Lake City",
+		"state": "UT",
+	},
+	"numbers": []interface{}{"one", "two", "three", "four", "five"},
+}
+
 type Convertable struct {
 	name string
 }
