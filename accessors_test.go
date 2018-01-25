8c1cb5005c048719d4e0dc0a203077924ff0b801
Ben Schumacher <hanzei@mailbox.org>
2018-01-25 23:32:30 +0100

    Move test to package objx_test

--- accessors_test.go
+++ accessors_test.go
@@ -1,166 +1,168 @@
-package objx
+package objx_test
 
 import (
 	"testing"
 
+	"github.com/stretchr/objx"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestAccessorsAccessGetSingleField(t *testing.T) {
-	current := Map{"name": "Tyler"}
+	m := objx.Map{"name": "Tyler"}
 
-	assert.Equal(t, "Tyler", current.Get("name").Data())
+	assert.Equal(t, "Tyler", m.Get("name").Data())
 }
 
 func TestAccessorsAccessGetSingleFieldInt(t *testing.T) {
-	current := Map{"name": 10}
+	m := objx.Map{"name": 10}
 
-	assert.Equal(t, 10, current.Get("name").Data())
+	assert.Equal(t, 10, m.Get("name").Data())
 }
 
 func TestAccessorsAccessGetDeep(t *testing.T) {
-	current := Map{
-		"name": Map{
+	m := objx.Map{
+		"name": objx.Map{
 			"first": "Tyler",
 			"last":  "Bunnell",
 		},
 	}
 
-	assert.Equal(t, "Tyler", current.Get("name.first").Data())
-	assert.Equal(t, "Bunnell", current.Get("name.last").Data())
+	assert.Equal(t, "Tyler", m.Get("name.first").Data())
+	assert.Equal(t, "Bunnell", m.Get("name.last").Data())
 }
 
 func TestAccessorsAccessGetDeepDeep(t *testing.T) {
-	current := Map{
-		"one": Map{
-			"two": Map{
-				"three": Map{
+	m := objx.Map{
+		"one": objx.Map{
+			"two": objx.Map{
+				"three": objx.Map{
 					"four": 4,
 				},
 			},
 		},
 	}
 
-	assert.Equal(t, 4, current.Get("one.two.three.four").Data())
+	assert.Equal(t, 4, m.Get("one.two.three.four").Data())
 }
 
 func TestAccessorsAccessGetInsideArray(t *testing.T) {
-	current := Map{
+	m := objx.Map{
 		"names": []interface{}{
-			Map{
+			objx.Map{
 				"first": "Tyler",
 				"last":  "Bunnell",
 			},
-			Map{
+			objx.Map{
 				"first": "Capitol",
 				"last":  "Bollocks",
 			},
 		},
 	}
 
-	assert.Equal(t, "Tyler", current.Get("names[0].first").Data())
-	assert.Equal(t, "Bunnell", current.Get("names[0].last").Data())
-	assert.Equal(t, "Capitol", current.Get("names[1].first").Data())
-	assert.Equal(t, "Bollocks", current.Get("names[1].last").Data())
+	assert.Equal(t, "Tyler", m.Get("names[0].first").Data())
+	assert.Equal(t, "Bunnell", m.Get("names[0].last").Data())
+	assert.Equal(t, "Capitol", m.Get("names[1].first").Data())
+	assert.Equal(t, "Bollocks", m.Get("names[1].last").Data())
 
-	assert.Nil(t, current.Get("names[2]").Data())
+	assert.Nil(t, m.Get("names[2]").Data())
 }
 
 func TestAccessorsGet(t *testing.T) {
-	current := Map{"name": "Tyler"}
+	m := objx.Map{"name": "Tyler"}
 
-	assert.Equal(t, "Tyler", current.Get("name").Data())
+	assert.Equal(t, "Tyler", m.Get("name").Data())
 }
 
 func TestAccessorsAccessSetSingleField(t *testing.T) {
-	current := Map{"name": "Tyler"}
+	m := objx.Map{"name": "Tyler"}
 
-	current.Set("name", "Mat")
-	current.Set("age", 29)
+	m.Set("name", "Mat")
+	m.Set("age", 29)
 
-	assert.Equal(t, current["name"], "Mat")
-	assert.Equal(t, current["age"], 29)
+	assert.Equal(t, m["name"], "Mat")
+	assert.Equal(t, m["age"], 29)
 }
 
 func TestAccessorsAccessSetSingleFieldNotExisting(t *testing.T) {
-	current := Map{
+	m := objx.Map{
 		"first": "Tyler",
 		"last":  "Bunnell",
 	}
 
-	current.Set("name", "Mat")
+	m.Set("name", "Mat")
 
-	assert.Equal(t, current["name"], "Mat")
+	assert.Equal(t, m["name"], "Mat")
 }
 
 func TestAccessorsAccessSetDeep(t *testing.T) {
-	current := Map{
-		"name": Map{
+	m := objx.Map{
+		"name": objx.Map{
 			"first": "Tyler",
 			"last":  "Bunnell",
 		},
 	}
 
-	current.Set("name.first", "Mat")
-	current.Set("name.last", "Ryer")
+	m.Set("name.first", "Mat")
+	m.Set("name.last", "Ryer")
 
-	assert.Equal(t, "Mat", current.Get("name.first").Data())
-	assert.Equal(t, "Ryer", current.Get("name.last").Data())
+	assert.Equal(t, "Mat", m.Get("name.first").Data())
+	assert.Equal(t, "Ryer", m.Get("name.last").Data())
 }
 
 func TestAccessorsAccessSetDeepDeep(t *testing.T) {
-	current := Map{
-		"one": Map{
-			"two": Map{
-				"three": Map{
-					"four": 4},
+	m := objx.Map{
+		"one": objx.Map{
+			"two": objx.Map{
+				"three": objx.Map{
+					"four": 4,
+				},
 			},
 		},
 	}
 
-	current.Set("one.two.three.four", 5)
+	m.Set("one.two.three.four", 5)
 
-	assert.Equal(t, 5, current.Get("one.two.three.four").Data())
+	assert.Equal(t, 5, m.Get("one.two.three.four").Data())
 }
 
 func TestAccessorsAccessSetArray(t *testing.T) {
-	current := Map{
+	m := objx.Map{
 		"names": []interface{}{"Tyler"},
 	}
-	current.Set("names[0]", "Mat")
+	m.Set("names[0]", "Mat")
 
-	assert.Equal(t, "Mat", current.Get("names[0]").Data())
+	assert.Equal(t, "Mat", m.Get("names[0]").Data())
 }
 
 func TestAccessorsAccessSetInsideArray(t *testing.T) {
-	current := Map{
+	m := objx.Map{
 		"names": []interface{}{
-			Map{
+			objx.Map{
 				"first": "Tyler",
 				"last":  "Bunnell",
 			},
-			Map{
+			objx.Map{
 				"first": "Capitol",
 				"last":  "Bollocks",
 			},
 		},
 	}
 
-	current.Set("names[0].first", "Mat")
-	current.Set("names[0].last", "Ryer")
-	current.Set("names[1].first", "Captain")
-	current.Set("names[1].last", "Underpants")
+	m.Set("names[0].first", "Mat")
+	m.Set("names[0].last", "Ryer")
+	m.Set("names[1].first", "Captain")
+	m.Set("names[1].last", "Underpants")
 
-	assert.Equal(t, "Mat", current.Get("names[0].first").Data())
-	assert.Equal(t, "Ryer", current.Get("names[0].last").Data())
-	assert.Equal(t, "Captain", current.Get("names[1].first").Data())
-	assert.Equal(t, "Underpants", current.Get("names[1].last").Data())
+	assert.Equal(t, "Mat", m.Get("names[0].first").Data())
+	assert.Equal(t, "Ryer", m.Get("names[0].last").Data())
+	assert.Equal(t, "Captain", m.Get("names[1].first").Data())
+	assert.Equal(t, "Underpants", m.Get("names[1].last").Data())
 }
 
 func TestAccessorsSet(t *testing.T) {
-	current := Map{"name": "Tyler"}
+	m := objx.Map{"name": "Tyler"}
 
-	current.Set("name", "Mat")
+	m.Set("name", "Mat")
 
-	assert.Equal(t, "Mat", current.Get("name").data)
+	assert.Equal(t, "Mat", m.Get("name").Data())
 }
