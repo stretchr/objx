25ac23c22bc4684c074710c78586f3fbc29ea439
Earncef Sequeira <earncef@earncef.com>
2018-04-09 11:09:03 +0200

    Fixed incorrect result for obj.Value().MSI() getter

--- type_specific_codegen_test.go
+++ type_specific_codegen_test.go
@@ -129,16 +129,17 @@ func TestCollectInter(t *testing.T) {
   Tests for MSI (map[string]interface{} and []map[string]interface{})
 */
 func TestMSI(t *testing.T) {
-  val := map[string]interface{}(map[string]interface{}{"name":"Tyler"})
-  m := objx.Map{"value": val, "nothing": nil}
+	val := map[string]interface{}(map[string]interface{}{"name": "Tyler"})
+	m := objx.Map{"value": val, "nothing": nil}
 
-  assert.Equal(t, val, m.Get("value").MSI())
-  assert.Equal(t, val, m.Get("value").MustMSI())
-  assert.Equal(t, map[string]interface{}(nil), m.Get("nothing").MSI())
-  assert.Equal(t, val, m.Get("nothing").MSI(map[string]interface{}{"name":"Tyler"}))
-  assert.Panics(t, func() {
-    m.Get("age").MustMSI()
-  })
+	assert.Equal(t, map[string]interface{}{"value": val, "nothing": nil}, m.Value().MSI())
+	assert.Equal(t, val, m.Get("value").MSI())
+	assert.Equal(t, val, m.Get("value").MustMSI())
+	assert.Equal(t, map[string]interface{}(nil), m.Get("nothing").MSI())
+	assert.Equal(t, val, m.Get("nothing").MSI(map[string]interface{}{"name": "Tyler"}))
+	assert.Panics(t, func() {
+		m.Get("age").MustMSI()
+	})
 }
 
 func TestMSISlice(t *testing.T) {
